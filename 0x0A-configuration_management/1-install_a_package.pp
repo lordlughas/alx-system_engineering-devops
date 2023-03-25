3a399d302b72f02bfbf06923311258de5f231202
Charles Lughas <lordlughas@gmail.com>
2023-03-25 02:47:16 -0700

    Hot fix task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Using Puppet, install flack from pip3
 
-exec { 'install python packages':
-  command => 'pip3 install flask',
-  ensure  => '2.1.0',
-  path    => ['/usr/bin/']
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
