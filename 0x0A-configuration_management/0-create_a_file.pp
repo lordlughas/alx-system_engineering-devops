515b1e6857a6f05b46cad19fa4c38a20a02a5351
Charles Lughas <lordlughas@gmail.com>
2023-03-25 01:48:35 -0700

    Create puppet file for task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creates a file in /tmp having file path /tmp/school
+file { '/tmp':
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet'
+}
