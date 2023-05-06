152157846c987a13cfb4b35f1332f875260ea3a7
Charles Lughas <lordlughas@gmail.com>
2023-05-06 22:05:44 +0000

    script to get information of user ToDo using REST API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Python script using REST API to return information of TODO list
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [td.get("title") for td in todos if td.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+    user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
+
+
